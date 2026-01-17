dd92401cf39ae7841d6fbc8e71a14fafd696f696
shivam980398 <shivayadu89@gmail.com>
2026-01-17 16:05:12 +0530

    Day1 of codeforces

---
+++ Day1/ConciseLongWords.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        string s;
+        cin >> s;
+        int len = s.length();
+
+        if (len > 10)
+        {
+            cout << s[0] << len - 2 << s[len - 1] << "\n";
+        }
+        else
+            cout << s << "\n";
+    }
+}
